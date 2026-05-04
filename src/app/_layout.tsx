e9c999bb70bf4eb942e684e65b4eb2b86a7e3367
Batrnan <tjdwo2323@naver.com>
2026-05-05 02:02:56 +0900

    [feat] 달력 ui 구현, 폰트 설정

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    'Pretendard-Light': require('../../assets/fonts/Pretendard-Light.ttf'),
+    'Pretendard-Regular': require('../../assets/fonts/Pretendard-Regular.ttf'),
+    'Pretendard-Medium': require('../../assets/fonts/Pretendard-Medium.ttf'),
+    'Pretendard-SemiBold': require('../../assets/fonts/Pretendard-SemiBold.ttf'),
+    'Pretendard-Bold': require('../../assets/fonts/Pretendard-Bold.ttf'),
+  });
+
+  if (!loaded) return null;
+
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false }} />
